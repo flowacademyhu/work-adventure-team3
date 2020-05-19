3f78dcdbee451c5118e82b61bf3cf416ece862c8
Braj Roland <brroland2@gmail.com>
2020-05-19 20:24:35 +0200

    basic welcome message and some pre-setup var.

--- adventure/index.js
+++ adventure/index.js
@@ -1,1 +1,15 @@
 // main program írás
+const map = require('./map');
+const move = require('./move');
+// const table = require'../require');
+// ne felejtsd el beolvasni az útirányt
+// ne felejtsd el táblát skiccelni, behívni, rajzolni
+
+const main = () => {
+  console.log('hello adventure!');
+  const position = arr[3][1];
+  console.log('you awaked in the middle of ', position);
+  console.log('if you want to move type one of the following orders: észak, dél, nyugat, kelet');
+  move.move();
+};
+main();
