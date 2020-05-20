40b61d64c52cbd591795e2bb74bc6670dd0daa8d
Kriván Zoltán <krivan.zoltan@gmail.com>
2020-05-20 17:44:30 +0200

    cell cordinatas change like chess table

--- adventure/map.js
+++ adventure/map.js
@@ -8,7 +8,7 @@ const generateMap = (width, height) => {
 
 const fillMap = (arr) => {
   const topLine = ['A', 'B', 'C', 'D', 'E', 'F'];
-  const sideLine = ['1 ', '2 ', '3 ', '4 ', '5 ', '6 '];
+  const sideLine = ['6 ', '5 ', '4 ', '3 ', '2 ', '1 '];
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
       arr[j][i] = topLine[i] + sideLine[j];
