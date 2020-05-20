0d7e5f45b156351b8154cf9657d4e480df5cb507
Kriván Zoltán <krivan.zoltan@gmail.com>
2020-05-20 17:36:35 +0200

    basic functions uploded, and fill the array

---
+++ adventure/map.js
@@ -0,0 +1,35 @@
+const generateMap = (width, height) => {
+  const arr = new Array(height);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(width);
+  }
+  return arr;
+};
+
+const fillMap = (arr) => {
+  const topLine = ['A', 'B', 'C', 'D', 'E', 'F'];
+  const sideLine = ['1 ', '2 ', '3 ', '4 ', '5 ', '6 '];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[j][i] = topLine[i] + sideLine[j];
+    }
+  }
+};
+
+const printMap = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      process.stdout.write(arr[i][j] + (j < arr[i].length - 1 ? ' ' : '\r\n'));
+    }
+  }
+};
+
+const arr = generateMap(6, 6);
+fillMap(arr);
+printMap(arr);
+
+module.exports = {
+  fillMap,
+  generateMap,
+  printMap
+};
